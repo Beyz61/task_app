9e476afc572bce4e905a56366afcfa026b7eb731
beyzanur <beyzanur.o@icloud.com>
2024-09-20 15:09:42 +0200

    add task

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_2.dart
@@ -4,6 +4,30 @@ class S4532 extends StatelessWidget {
   const S4532({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const MyStatelessWidget1();
+  }
+}
+
+class MyStatelessWidget1 extends StatelessWidget {
+  const MyStatelessWidget1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("App Akademie"),
+        Text(
+          "App Akademie",
+          style: TextStyle(
+              fontSize: 16, fontWeight: FontWeight.bold, color: Colors.blue),
+        ),
+        Text(
+          "App Akademie",
+          style: TextStyle(
+              fontSize: 10, fontStyle: FontStyle.italic, color: Colors.green),
+        ),
+      ],
+    );
   }
 }
